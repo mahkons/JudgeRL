bee4a6331866181bb043a1d566ebc7ef15cdce86
Makhnev Konstantin <mah.kons@gmail.com>
2021-03-29 00:40:14 +0300

    add ugly history

--- PPO.py
+++ PPO.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 
 from networks import Actor, Critic
-from params import GAMMA, LAMBDA, AGENT_CLIP, JUDGE_KL_BETA, ENTROPY_COEF, BATCHES_PER_UPDATE, BATCH_SIZE, VALUE_COEFF
+from params import GAMMA, LAMBDA, CLIP, KL_BETA, ENTROPY_COEF, BATCHES_PER_UPDATE, BATCH_SIZE, VALUE_COEFF
 
 def compute_lambda_returns_and_gae(trajectory):
     lambda_returns = []
@@ -32,31 +32,31 @@ class PPO():
 
     def _calc_loss(self, state, action, old_log_prob, expected_values, gae):
         new_log_prob, action_distr = self.actor.compute_proba(state, action)
-        with torch.no_grad():
-            old_log_prob, old_action_distr = self.old_actor.compute_proba(state, action)
         state_values = self.critic.get_value(state).squeeze(1)
 
         critic_loss = ((expected_values - state_values) ** 2).mean()
 
         unclipped_ratio = torch.exp(new_log_prob - old_log_prob)
         if self.clip:
-            clipped_ratio = torch.clamp(unclipped_ratio, 1 - AGENT_CLIP, 1 + AGENT_CLIP)
+            clipped_ratio = torch.clamp(unclipped_ratio, 1 - CLIP, 1 + CLIP)
             actor_loss = -torch.min(clipped_ratio * gae, unclipped_ratio * gae).mean()
             entropy_loss = -action_distr.entropy().mean()
         else:
-            actor_loss = -(unclipped_ratio * gae).mean() - JUDGE_KL_BETA * torch.distributions.kl.kl_divergence(old_action_distr, action_distr).mean()
+            with torch.no_grad():
+                old_log_prob, old_action_distr = self.old_actor.compute_proba(state, action)
+            actor_loss = -(unclipped_ratio * gae).mean() - KL_BETA * torch.distributions.kl.kl_divergence(old_action_distr, action_distr).mean()
             entropy_loss = -action_distr.entropy().mean()
 
 
         return VALUE_COEFF * critic_loss + actor_loss + entropy_loss * ENTROPY_COEF
 
 
-    def update(self, trajectories):
+    def update(self, trajectories, history=None, history_optimizer=None):
         trajectories = map(compute_lambda_returns_and_gae, trajectories)
         transitions = sum(trajectories, []) # Turn a list of trajectories into list of transitions
 
         state, action, old_log_prob, target_value, advantage = zip(*transitions)
-        state = np.array(state)
+        state = torch.stack(state)
         action = np.array(action)
         old_log_prob = np.array(old_log_prob)
         target_value = np.array(target_value)
@@ -64,8 +64,16 @@ class PPO():
         
         
         for _ in range(BATCHES_PER_UPDATE):
+            if history_optimizer is not None:
+                tofit = torch.stack([sa for sa in history[1]])
+                _, hc = history[0](tofit.unsqueeze(1))
+                hc = torch.cat(hc).reshape(-1).unsqueeze(0)
+                hc = hc.expand(BATCH_SIZE, hc.shape[1])
+
             idx = np.random.randint(0, len(transitions), BATCH_SIZE) # Choose random batch
-            s = torch.tensor(state[idx], dtype=torch.float)
+            s = state[idx]
+            if history_optimizer is not None:
+                s = torch.cat([s, hc], axis=1)
             a = torch.tensor(action[idx], dtype=torch.float)
             op = torch.tensor(old_log_prob[idx], dtype=torch.float) # Log probability of the action in state s.t. old policy
             v = torch.tensor(target_value[idx], dtype=torch.float) # Estimated by lambda-returns 
@@ -75,7 +83,17 @@ class PPO():
 
             self.actor_optim.zero_grad()
             self.critic_optim.zero_grad()
-            loss.backward()
+            if history_optimizer is not None: history_optimizer.zero_grad()
+
+            if history_optimizer is not None:
+                loss.backward(retain_graph=True)
+            else:
+                loss.backward()
+
+            if history_optimizer is not None:
+                torch.nn.utils.clip_grad_norm_(history[0].parameters(), 1.)
+
+            if history_optimizer is not None: history_optimizer.step()
             self.actor_optim.step()
             self.critic_optim.step()
 
--- train.py
+++ train.py
@@ -6,9 +6,10 @@ import numpy as np
 import torch
 from torch import nn
 import random
+from collections import deque
 
 from PPO import PPO
-from params import  ENV_NAME, MIN_EPISODES_PER_UPDATE, MIN_TRANSITIONS_PER_UPDATE, ITERATIONS, LR, JUDGE_LR
+from params import  ENV_NAME, MIN_EPISODES_PER_UPDATE, MIN_TRANSITIONS_PER_UPDATE, ITERATIONS, LR, JUDGE_LR, HISTORY_SZ, HISTORY_LEN, HISTORY_THINNING
 
 
 def evaluate_policy(env, agent, episodes):
@@ -25,21 +26,28 @@ def evaluate_policy(env, agent, episodes):
     return returns
    
 
-def sample_episode(env, agent, judge):
+def sample_episode(env, agent, judge, history):
     s = env.reset()
     done = False
     judge_trajectory = list()
     trajectory = list()
+
+    with torch.no_grad():
+        tofit = torch.stack([sa for sa in history[1]])
+        _, hc = history[0](tofit.unsqueeze(1))
+        hc = torch.cat(hc).detach().reshape(-1).numpy()
+
     while not done:
         a, pa, p = agent.act(s)
-        ja, jpa, jp = judge.act(np.concatenate([s, a]))
+        ja, jpa, jp = judge.act(np.concatenate([s, a, hc]))
 
         v = agent.get_value(s)
         ns, r, done, _ = env.step(a)
 
-        judge_trajectory.append((np.concatenate([s, a]), jpa, r, jp, v))
-        trajectory.append((s, pa, ja, p, v))
 
+        judge_trajectory.append((torch.cat([torch.from_numpy(s), torch.from_numpy(a)]), jpa, r, jp, v))
+        trajectory.append((torch.from_numpy(s), pa, ja, p, v))
+        
         s = ns
     return trajectory, judge_trajectory
 
@@ -47,8 +55,12 @@ def sample_episode(env, agent, judge):
 def train():
     env = gym.make(ENV_NAME)
 
-    judge = PPO(state_dim=env.observation_space.shape[0] + env.action_space.shape[0], action_dim=1, clip=True, lr=JUDGE_LR)
+    history = (torch.nn.LSTM(env.observation_space.shape[0] + env.action_space.shape[0] + 1, HISTORY_SZ, num_layers=1), deque(maxlen=HISTORY_LEN))
+    judge = PPO(state_dim=env.observation_space.shape[0] + env.action_space.shape[0] + 2 * HISTORY_SZ, action_dim=1, clip=True, lr=JUDGE_LR)
     ppo = PPO(state_dim=env.observation_space.shape[0], action_dim=env.action_space.shape[0], clip=True, lr=LR)
+
+    history_optimizer = torch.optim.Adam(history[0].parameters(), lr=1e-4)
+    history[1].append(torch.zeros(env.observation_space.shape[0] + env.action_space.shape[0] + 1))
 
     state = env.reset()
     episodes_sampled = 0
@@ -62,7 +74,7 @@ def train():
         steps_ctn = 0
         
         while len(trajectories) < MIN_EPISODES_PER_UPDATE or steps_ctn < MIN_TRANSITIONS_PER_UPDATE:
-            traj, jtraj = sample_episode(env, ppo, judge)
+            traj, jtraj = sample_episode(env, ppo, judge, history)
             steps_ctn += len(traj)
             trajectories.append(traj)
             judge_trajectories.append(jtraj)
@@ -71,7 +83,16 @@ def train():
         steps_sampled += steps_ctn
 
         ppo.update(trajectories)        
-        judge.update(judge_trajectories)
+        judge.update(judge_trajectories, history, history_optimizer) # ugly yeah
+
+
+        cnt = 0
+        for tt in zip(judge_trajectories, trajectories):
+            for jt, t in zip(*tt):
+                if cnt % HISTORY_THINNING == 0:
+                    history[1].append(torch.cat([jt[0], torch.tensor(t[2])]))
+                cnt += 1
+
 
         s = sum([t[2] for tt in judge_trajectories for t in tt]) / len(judge_trajectories)
         print("lol {}".format(s))
